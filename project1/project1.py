e7b5ebc3ac04dd15e560ef4887491f7786d1b2ba
jtaylorcpp <jtaylorcpp@gmail.com>
2014-01-08 23:19:00 -0800

    added a sorting function, along with an algorith that builds each nodes logical equation
    calls back earlier node equations for end node equations

--- project1/project1.py
+++ project1/project1.py
@@ -10,20 +10,40 @@ commands = {'AND': '&', 'OR': '|', 'XOR': '^'}
 file = ['6\tCARRY\tOR\t3\t4\t5', '1\tXOR1\tXOR\tA\tB', '3\tAND2\tAND\tA\tC', '4\tAND1\tAND\tA\tB', '5\tAND3\tAND\tB\tC', '2\tSUM\tXOR\t1\tC']
 
 # Load file lines into array.
-args = [None]*len(file)
-for y in range(0, len(file)):
-    args[y] = file[y].split();
+def read_file (file):
+    args = [None]*len(file)
+    for y in range(0, len(file)):
+        args[y] = file[y].split();
+    return args
 
 # Organize file lines by gate index.
-nodes = [None]*len(file)
-result = [None]*len(file)
-for z in range(0, len(args)):
-    nodes[int(''.join(args[z][0]))-1] = args[z];
+def organize_array(array):
+    for x in range (1,len(array)):
+        node_number = array[x-1][0]
+        if (eval(node_number) != x):
+            swap = array[eval(node_number)-1]
+            array[eval(node_number)-1] = array[x-1]
+            array[x-1] = swap
+
+# Creates logic function for each node - use only after read_file and sort
+def create_logic_functions(array):
+    for x in range (0,len(array)-1):#finds number of total node lines
+        logic_equations[x]=array[x][3]#sets initial input to first in list (position 4 from sorted list)
+        for y in range (4,len(array[x])-1):#from position 4 forward - include inputs
+            if (array[x][y].isalpha()):# is input is alphabetic it is added directly to equation
+                logic_equation[x]=logic_equation[x] + commands[array[x][2]] + array[x][y]
+            else:#else, in () add equation from earlier node
+                logic_equation[x]=logic_equation[x] + commands[array[x][2]] + "(" + loggic_equation[eval(array[x][y])]+ ")"
+    return logic_equation
 
 # Print nodes array to check organization.
 for z in range(0, len(nodes)):
     print (nodes[z])
 
+#print array
+def print_array(array):
+    for x in range (0,len(array)):
+        print(array[x])
 
 # From this point on, everything is still in the works.
 A = 1;
