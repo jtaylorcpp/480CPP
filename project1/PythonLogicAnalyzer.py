2a4276aad240f5ebf0fb754cb59a3a1cd798477d
jtaylorcpp <jtaylorcpp@gmail.com>
2014-01-08 19:45:12 -0800

    check to see that master and JTP are seperate

--- project1/PythonLogicAnalyzer.py
+++ project1/PythonLogicAnalyzer.py
@@ -2,3 +2,5 @@
 #please comment on all additions and chnages to code before commit
 #if there is a bug foundplease comment on the bug and commit with the bug issue in the notes
 #	then recommit before fixing
+
+#algorithm for reading type of logic and creating login equation
